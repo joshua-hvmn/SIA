79e1c5d99e2bb9c3f3ae6cb0c94887848c12708e
joshua-hvmn <secondjoshua1@gmail.com>
2025-12-24 01:22:34 -0800

    Setup script working

--- setup.sh
+++ setup.sh
@@ -1,21 +1,47 @@
 #!/bin/bash
 
-echo "Welcome to SIA - Initial Setup"
-echo "System Architecture:"
+# VARIABLES:
+
 options=("CPU Only" "Nvidia GPU" "AMD GPU")
-select opt in "${options[@]}"; do
-    case "$opt" in
+fileNames=("compose.cpu.yaml" "compose.nvidia.yaml" "compose.amd.yaml")
+
+# FUNCTIONS:
+
+editFunc () {
+    mv ${fileNames[$1]} "compose.yaml"
+    unset 'fileNames[$1]'
+    fileNames=("${fileNames[@]}")
+    for i in "${!fileNames[@]}"; do
+        mv "$fileNames" "Archive/"
+        unset 'fileNames[$i]'
+        fileNames=("${fileNames[@]}")
+    done
+}
+
+executeFunc () {
+    case "$1" in
         "CPU Only")
-            echo "CPU"
+            editFunc 0
+            exit
             ;;
         "Nvidia GPU")
-            echo "Nvidia"
+            editFunc 1
+            exit
             ;;
         "AMD GPU")
-            echo "AMD"
+            editFunc 2
+            exit
             ;;
         *)
-            echo "Invalid choice, try again."
+            echo "Invalid choice, select a number between 1 and 3."
             ;;
     esac
+}
+
+# MAIN SECTION:
+
+echo "Welcome to SIA - Initial Setup"
+echo "System Architecture:"
+select opt in "${options[@]}"; do
+executeFunc "$opt"
 done
