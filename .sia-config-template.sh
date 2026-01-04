d4e28ac1e975155c23855ecac1e0771498bd2291
joshua-hvmn <secondjoshua1@gmail.com>
2026-01-04 14:35:39 -0800

    Add: MIT license and warnings to config template

---
+++ .sia-config-template.sh
@@ -0,0 +1,14 @@
+# |----------------------------------------------------------------------------|
+# |                        SIA Management Tool - Config                        |
+# |----------------------------------------------------------------------------|
+# |              SPDX-License-Identifier: MIT                                  |
+# |              SPDX-FileCopyrightText: 2025-2026 Joshua Haveman              |
+# |  This tool is released under the MIT License: modify & distribute freely.  |
+# |----------------------------------------------------------------------------|
+#
+# DO NOT DELETE THE CONFIG TEMPLATE.
+# Secret Keys and hostname are stored in the local file 'sia-config.sh'.
+# Do not accidentally commit the local copy.
+
+
+#!/usr/bin/env bash
